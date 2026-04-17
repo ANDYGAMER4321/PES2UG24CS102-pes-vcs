9ed61b67cc0e1ec671cdec73f9810e80e26edcd6
ANDYGAMER4321 <ayushmandey35@gmail.com>
2026-04-18 05:28:36 +0530

    feat(commit): link commit serialization to object store writing

--- commit.c
+++ commit.c
@@ -218,5 +218,13 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     if (commit_serialize(&c, &data, &len) < 0) {
         return -1;
     }
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) < 0) {
+        free(data);
+        return -1;
+    }
+    free(data);
+
+    
     return 0;
 }
